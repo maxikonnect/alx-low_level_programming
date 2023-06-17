40cbddcd7a2caf016b1c6c530f239fb7994910ff
maxikonnect <tribillio@gmail.com>
2023-06-16 20:14:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main -This code  Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (code Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
