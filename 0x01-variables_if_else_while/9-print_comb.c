598f6c6192d89821a82146afe5d860a3ed4a0dcc
maxikonnect <tribillio@gmail.com>
2023-06-16 21:41:51 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (code Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
