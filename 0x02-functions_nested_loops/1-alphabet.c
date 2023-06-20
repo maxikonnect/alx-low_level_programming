d0dadaeee277ebeb642bce46adf5b7f5867d4ba2
maxikonnect <tribillio@gmail.com>
2023-06-20 14:20:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char vim;
+
+	for (vim = 'a'; vim <= 'z'; vim++)
+	{
+		_putchar(vimm);
+	}
+
+	_putchar('\n');
+}
