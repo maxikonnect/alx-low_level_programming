94a0043d8f5fc69286d6991b6a1af3899a8e9ddb
maxikonnect <tribillio@gmail.com>
2023-06-20 15:04:40 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char letter;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+	}
+}
+
