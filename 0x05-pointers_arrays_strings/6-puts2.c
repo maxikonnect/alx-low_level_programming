a620380f983dd239d1bc1883cf5e4d00544f6a8c
maxikonnect <tribillio@gmail.com>
2023-06-26 09:29:48 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 -  prints every other character of a string, starting with the first character
+ * puts2 -  prints every other character of a string,first character starts
  *@str: the string to use.
  */
 
