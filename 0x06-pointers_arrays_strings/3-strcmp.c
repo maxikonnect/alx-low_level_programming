4378ffff8c6ee940588cb4253af54f0be78134e1
maxikonnect <tribillio@gmail.com>
2023-06-29 09:30:37 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strcmp - a function that compares two strings
+ *
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
+ *
+ * Return: 0.
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int res = 0;
+
+	while (*s1 == *s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 != s2)
+		res = *s1 - *s2;
+
+	return (res);
+}
